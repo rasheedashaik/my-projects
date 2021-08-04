0d46b4fff970b66cc609e83f5d6acea1bbb37792
rasheedashaik <88425178+rasheedashaik@users.noreply.github.com>
2021-08-04 13:27:55 +0530

    Add files via upload

---
+++ photo.js
@@ -0,0 +1,104 @@
+$(function() {
+	var THUMBNAIL_WIDTH = 140,
+		GALLERY = $('#slideshow');
+	GALLERY.find('.thumb').on('click', function() {
+		loadClickedImage($(this).data('thumb-id'));
+	});
+	GALLERY.find('#prev-btn').on('click', function() {
+		slidePrev();
+	});
+	GALLERY.find('#next-btn').on('click', function() {
+		slideNext();
+	});
+	$(document).keydown(function(e){
+		switch(e.keyCode) {
+			case 37:
+				slidePrev();
+				break;
+			case 39:
+				slideNext();
+				break;
+			default:
+				break;
+		}
+	});
+	var slideNext = function() {
+	
+	    var active = GALLERY.find('.img-wrapper.active');
+
+		if (active.length === 0) { 
+			active = GALLERY.find('.img-wrapper:last');
+		}
+	    loadNextImage(active);
+	};
+	var loadNextImage = function(active) {
+		var next =  active.next(".img-wrapper").length ? active.next(".img-wrapper") : GALLERY.find('.img-wrapper:first'),
+			nextThumb = GALLERY.find('[data-thumb-id="' + next.data('img-id') + '"]');
+		GALLERY.find('.thumb').removeClass('active');
+		nextThumb.addClass('active');
+		active.addClass('last-active');
+		scrollThumbnails(nextThumb);
+		next.css({opacity: 0.0})
+			.addClass('active')
+			.animate({opacity: 1.0}, 1000, function() {
+				active.removeClass('active last-active');
+			});
+	};
+	var slidePrev = function() {
+		var active = GALLERY.find('.img-wrapper.active');
+
+		if (active.length === 0) {
+			active = GALLERY.find('.img-wrapper:last');
+		}
+		loadPrevImage(active);
+	};
+	var loadPrevImage = function(active) {
+		var prev =  active.prev(".img-wrapper").length ? active.prev(".img-wrapper") : GALLERY.find('.img-wrapper:last'),
+			prevThumb = GALLERY.find('[data-thumb-id="' + prev.data('img-id') + '"]');
+		GALLERY.find('.thumb').removeClass('active');
+		prevThumb.addClass('active');
+		active.addClass('last-active');
+		scrollThumbnails(prevThumb);
+		prev.css({opacity: 0.0})
+			.addClass('active')
+			.animate({opacity: 1.0}, 1000, function() {
+				active.removeClass('active last-active');
+			});
+	};
+	var loadClickedImage = function(id) {
+		var image =  GALLERY.find('[data-img-id="' + id + '"]'),
+			imgThumb = GALLERY.find('[data-thumb-id="' + id + '"]'),
+			currActive = GALLERY.find('.img-wrapper.active');
+		GALLERY.find('.thumb').removeClass('active');
+		currActive.addClass('last-active').removeClass('active');
+		imgThumb.addClass('active');
+		scrollThumbnails(imgThumb);
+		image.css({opacity: 0.0})
+			.addClass('active')
+			.animate({opacity: 1.0}, 1000, function() {
+				currActive.removeClass('last-active');
+			});
+	};
+	var scrollThumbnails = function(thumb) {
+		var offset, 
+			first, 
+			x = thumb.position().left + parseInt(thumb.css('margin-left'), 10);
+		if(x < 0) {
+			first = GALLERY.find('.thumb:first');
+			offset = parseInt(first.css('margin-left'), 10) - x;
+			first.animate({
+				marginLeft: offset
+			}, 1000);
+		} else {
+			x = thumb.position().left;
+			var currOffset = ( x + THUMBNAIL_WIDTH ) - thumb.parent().width();
+			if(currOffset > 0) {
+				first = GALLERY.find('.thumb:first');
+				offset = parseInt(first.css('margin-left'), 10) - currOffset;
+				first.animate({
+					marginLeft: offset
+				}, 1000);
+			}
+		}
+	};
+}());
